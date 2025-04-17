42f8fb79cc22c5cc3c7d302a74365cfe2ce1abc4
Brian 'bdougie' Douglas <brian@opensauced.pizza>
2025-04-17 12:35:04 -0700

    adds yolo

--- src/lib/repo-stats-context.tsx
+++ src/lib/repo-stats-context.tsx
@@ -1,10 +1,11 @@
 import React, { createContext } from "react";
-import type { RepoStats, LotteryFactor } from "@/lib/types";
+import type { RepoStats, LotteryFactor, DirectCommitsData } from "@/lib/types";
 
 // Context to share data between tabs
 interface RepoStatsContextType {
   stats: RepoStats;
   lotteryFactor: LotteryFactor | null;
+  directCommitsData: DirectCommitsData | null;
   includeBots: boolean;
   setIncludeBots: (value: boolean) => void;
 }
@@ -12,6 +13,7 @@ interface RepoStatsContextType {
 export const RepoStatsContext = createContext<RepoStatsContextType>({
   stats: { pullRequests: [], loading: true, error: null },
   lotteryFactor: null,
+  directCommitsData: null,
   includeBots: false,
   setIncludeBots: () => {},
 });
