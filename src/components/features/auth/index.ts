8843d12da2dcb85299e3aefc7beb39e5681de0d1
Brian Douglas <bdougie@users.noreply.github.com>
2025-06-21 06:54:03 -0700

    fix: protect the debug page (#103)
    * style debug
    
    * clean up /dev

--- src/components/features/auth/index.ts
+++ src/components/features/auth/index.ts
@@ -1,5 +1,6 @@
 export { AuthButton } from './auth-button';
 export { LoginDialog } from './login-dialog';
+export { ProtectedRoute } from './protected-route';
 export { default as LoginPage } from './login-page';
 export { default as DebugAuthPage } from './debug-auth-page';
 export { default as LoginDialogTest } from './login-dialog-test';
